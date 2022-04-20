0d2c51f91bfc8003e6f284c4aac7e6191f3c3404
teri0411 <goaheadman@naver.com>
2022-04-20 18:18:53 +0900

    오타수정

--- alb.tf
+++ alb.tf
@@ -12,20 +12,21 @@ resource "aws_lb" "terraform-alb" {
 
 resource "aws_lb_target_group" "tg-terraform2" {
   name     = "tg-terraform2"
-  port     = "80"
+  port     = 80
   protocol = "HTTP"
   vpc_id   = "${aws_vpc.terraform-vpc-vpc.id}"
   health_check {
   interval            = 30
-  path                = "/index.html"
+  path                = "/"
   healthy_threshold   = 3
   unhealthy_threshold = 3
   }
 }
 
+
 resource "aws_lb_listener" "alb-listener" {
   load_balancer_arn = aws_lb.terraform-alb.arn
-  port              = "80"
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
--- asg.tf
+++ asg.tf
@@ -6,13 +6,13 @@ resource "aws_launch_template" "iac-launch-temp" {
 }
 
 resource "aws_autoscaling_group" "terraform-autoscaling" {
-    availability_zones = ["us-west-1b", "us-west-1c"]
     desired_capacity          = 2
     health_check_grace_period = 200
     health_check_type         = "EC2"
     max_size                  = 10
     min_size                  = 2
     name                      = "terraform-autoscaling"
+    vpc_zone_identifier       = [ aws_subnet.subnet-terraform-vpc-subnet-public2-us-west-1b.id, aws_subnet.subnet-terraform-vpc-subnet-public1-us-west-1c.id]
 
 
 
@@ -25,3 +25,12 @@ resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = "${aws_autoscaling_group.terraform-autoscaling.id}"
   alb_target_group_arn = "${aws_lb_target_group.tg-terraform2.arn}"
 } 
+
+resource "aws_autoscaling_policy" "custom-cpu-policy" {
+  name                   = "custom-cpu-policy"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.terraform-autoscaling.name
+  policy_type            = "SimpleScaling"
+}
